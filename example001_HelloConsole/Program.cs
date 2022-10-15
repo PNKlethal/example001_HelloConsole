9c2f1bb385bc251cf311dd734a0f331b7da2f0b9
PNKlethal <seregaa99@bk,ru>
2022-10-15 16:54:49 +0600

    add example002_HelloUser

--- example001_HelloConsole/Program.cs
+++ example001_HelloConsole/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("2 Hello, World!");
+﻿Console.WriteLine("Введи ваше имя");
+string username = Console.ReadLine();
+Console.WriteLine("Привет,");
+Console.WriteLine(username);
