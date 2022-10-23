f7bbd39e1585fd34892a85da0775669fd527adee
PNKlethal <seregaa99@bk,ru>
2022-10-23 16:38:36 +0600

    add example005_condition

---
+++ example005_condition/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write ("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower == "яна")
+{
+    Console.WriteLine ("Я тебя люблю!");
+}
+else
+{
+    Console.Write ("Привет, ");
+    Console.WriteLine (username);
+}
